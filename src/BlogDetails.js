86edfc01e80ea31c3140dd0d02ae438aa8a0b051
Wisdom okorie <91771635+Star-ddog@users.noreply.github.com>
2023-02-16 07:42:40 +0100

    Update BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,11 +3,11 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id} = useParams();
-    const { data: blog,error, ispending} = useFetch(' http://localhost:8000/blogs/' + id);
+    const { data: blog,error, ispending} = useFetch('https://blog-api-5vt7.onrender.com/blogs/' + id);
     const navigate = useNavigate();
 
     const handleClick =() => {
-        fetch(' http://localhost:8000/blogs/' + blog.id,{
+        fetch('https://blog-api-5vt7.onrender.com/blogs/' + blog.id,{
             method: 'DELETE'
         }).then(() =>{
             navigate('/');
